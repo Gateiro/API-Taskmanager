a160f650af6a6a12ca46ebae1d84cad5262533e7
MATHEUS RODRIGUES VIEIRA <matheus.rvieira5@senacsp.edu.br>
2025-07-31 22:01:54 -0300

    Atualizações DB e node

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors());
+app.use(cors()); //Módulo que permite requisições de diferentes origens.
 app.use(express.json());
 
 const tarefaRoutes = require('./routes/tarefaRoutes');
